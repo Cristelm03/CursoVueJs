074dbc11d8170e710170135da11d04aed3827143
Cristelm03 <fmoralesl5@miumg.edu.gt>
2025-09-04 22:10:08 -0600

    Proyecto inicial con propiedades computadas y metodos

---
+++ src/HolaUsuario.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input v-model="nombre" placeholder="Escribe tu nombre" />
+
+    <!-- Usando propiedad computada -->
+    <p>{{ mensajeBienvenida }}</p>
+
+    <!-- Usando método (para comparar) -->
+    <p>{{ obtenerBienvenida() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nombre: ''
+    }
+  },
+  computed: {
+    mensajeBienvenida() {
+      console.log("Computed ejecutado ✅");
+      return this.nombre
+        ? `Bienvenido, ${this.nombre}!`
+        : "Bienvenido, invitado!";
+    }
+  },
+  methods: {
+    obtenerBienvenida() {
+      console.log("Método ejecutado ⚡");
+      return this.nombre
+        ? `Bienvenido, ${this.nombre}!`
+        : "Bienvenido, invitado!";
+    }
+  }
+}
+</script>
